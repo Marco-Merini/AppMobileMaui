c36ce94f58f0e766afc6e790250e491e98b0e4b8
Marco-Merini <marcoleonemerini@gmail.com>
2025-04-01 13:28:32 -0300

    Codigo_Produto alterado para 5 digitos e Numero_Pedido alterado para codigo do tipo de codigo de barras

--- AppMobileMaui/Data/PedidoService.cs
+++ AppMobileMaui/Data/PedidoService.cs
@@ -14,11 +14,11 @@ public class PedidoService : IPedidosService
         await Task.Delay(1000);
         var pedidos = new[]
         {
-        new Pedido() {Numero_Pedido = "058956", Nome_Cliente = "Eduardo Vargas", Data_Pedido = "12/10/2001"},
-        new Pedido() {Numero_Pedido = "453875", Nome_Cliente = "Silvio Santos", Data_Pedido = "12/10/2001"},
-        new Pedido() {Numero_Pedido = "924875", Nome_Cliente = "Neymar Jr", Data_Pedido = "12/10/2001"},
-        new Pedido() {Numero_Pedido = "152348", Nome_Cliente = "Roberto Dinamite", Data_Pedido = "12/10/2001"},
-        new Pedido() {Numero_Pedido = "810254", Nome_Cliente = "Paulo da Silva", Data_Pedido = "12/10/2001"},
+        new Pedido() {Numero_Pedido = "7891000248768", Nome_Cliente = "Eduardo Vargas", Data_Pedido = "12/10/2001"},
+        new Pedido() {Numero_Pedido = "7897947612907", Nome_Cliente = "Silvio Santos", Data_Pedido = "21/10/2023"},
+        new Pedido() {Numero_Pedido = "1520277195852", Nome_Cliente = "Neymar Jr", Data_Pedido = "28/07/2008"},
+        new Pedido() {Numero_Pedido = "2565972751651", Nome_Cliente = "Roberto Dinamite", Data_Pedido = "05/10/1977"},
+        new Pedido() {Numero_Pedido = "4723843078320", Nome_Cliente = "Paulo da Silva", Data_Pedido = "03/11/2000"},
         };
         return pedidos.ToList();
     }
